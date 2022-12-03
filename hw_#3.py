22d494db5677cb895a5ac7cae91f027dad955076
KBIGGY <nikolaicev1990@gmail.com>
2022-12-03 18:42:56 +0300

    added hw_#3.py and 'result_file.txt'

---
+++ hw_#3.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 17:50:05 2022
+
+@author: LocalAdmin
+"""
+import os
+
+adrr = os.getcwd()
+files = os.listdir(os.chdir(f'{adrr}\sorted'))
+result_file_name = 'result_file.txt'
+
+
+def amount_lines(files):
+    count_dict = {}
+    for i in files:
+        if i != result_file_name:
+            count = 0
+            with open(i, 'r', encoding='utf-8') as f:
+                for line in f:
+                    count += 1
+                count_dict[i] = count
+    return count_dict
+
+def result_file(files_per_lines, result_file_name):
+    sorted_values = sorted(list(files_per_lines.values()))
+    res_file = open(result_file_name, 'w', encoding='utf-8')
+    for i in sorted_values:
+        for file, len_file in files_per_lines.items():
+            if len_file == i:
+                res_file.write(file + '\n')
+                res_file.write(str(len_file) + '\n')
+                with open(file, 'r', encoding='utf-8') as f:
+                    for line in f:
+                        res_file.write(line + '\n')
+        
+    return res_file
+                        
+
+files_per_lines = amount_lines(files)
+result_file(files_per_lines, result_file_name)
+
+
+
+
+
+    
